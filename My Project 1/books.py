3ff3c0d812f672d43e1f463674ed20f80c448d5b
Muzaffar <muzaffarmurodogli@gmail.com>
2026-06-30 17:51:28 +0500

    Add endpoints to retrieve books by title and category

--- My Project 1/books.py
+++ My Project 1/books.py
@@ -43,3 +43,18 @@ BOOKS = [
 @app.get("/books")
 def read_all_books():
     return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+
+            return book
+
+@app.get("/books/")
+def get_books_by_category(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
